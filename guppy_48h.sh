5d466157f15ea1868c862c440a78c9c836cc2e20
Tomasz Mądry <tke.madry@gmail.com>
2024-02-26 19:09:46 +0100

    updated paths

--- guppy_48h.sh
+++ guppy_48h.sh
@@ -5,19 +5,27 @@
 #SBATCH --time=48:00:00
 #SBATCH --mail-type=BEGIN,END
 
+# Unload all loaded modulefiles
+module purge
+
 # Load CUDA module
 module load cuda/11.8.0_520.61.05
 
-# Test modules
-module avail > /mnt/storage_4/home/klug3/pl0203-01/project_data/scripts/modules.txt
+# Set absolute PATHs
+export PATH=${HOME}${SLURM_JOB_ACCOUNT}
 
-# Check CUDA version
-nvcc --version >> /mnt/storage_4/home/klug3/pl0203-01/project_data/scripts/modules.txt
+export GUPPY_PATH=${PATH}/project_data/soft/ont-guppy/bin/guppy_basecaller
+export ABS_INPUT_DIR=${PATH}/${INPUT_DIR}
+export ABS_OUTPUT_DIR=${PATH}/${OUTPUT_DIR}
+export ABS_CONFIG=${PATH}/project_data/soft/ont-guppy/data/${CONFIG}
+
+# Check loaded CUDA version
+nvcc --version 
 
 # Perform calculations
-~/pl0203-01/project_data/soft/ont-guppy/bin/guppy_basecaller -i ${INPUT_DIR} \
---save_path ${OUTPUT_DIR} \
--c ${CONFIG} \
+$GUPPY_PATH -i ${ABS_INPUT_DIR} \
+--save_path ${ABS_OUTPUT_DIR} \
+-c ${ABS_CONFIG} \
 --min_qscore 9 \
 --compress_fastq \
 -x cuda:all:100% \
